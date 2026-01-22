47c94ac3052aee8f102609e195a4fdab65c45b6a
Cellenite <cellenite@gmail.com>
2026-01-22 12:59:24 +0800

    Add the ability to store and display text

--- src/main/java/Aerith.java
+++ src/main/java/Aerith.java
@@ -3,13 +3,27 @@ import java.util.Scanner;
 public class Aerith {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] items = new String[100];
+        int itemCount = 0;
 
         System.out.println("\n. ⚬ ✧ ○\n");
         System.out.println("✧ Greetings mage, I am Aerith! ✧\n");
 
         String input = scanner.nextLine();
         while (!input.equals("bye")) {
-            System.out.println("✧ " + input + " ✧\n");
+            if (input.equals("list")) {
+                // List items
+                System.out.println("✧ ✧ ✧");
+                for (int i = 0; i < itemCount; i++) {
+                    System.out.println((i+1) + ". " + items[i]);
+                }
+                System.out.println("✧ ✧ ✧\n");
+            } else {
+                // Add item
+                System.out.println("✧ Added: " + input + " ✧\n");
+                items[itemCount] = input;
+                itemCount++;
+            }
             input = scanner.nextLine();
         }
 
