f09dfc72a7bf902af425bba2b6560dc107b77ae7
Cellenite <cellenite@gmail.com>
2026-01-22 12:46:36 +0800

    Make the chatbot echo the user's inputs

--- src/main/java/Aerith.java
+++ src/main/java/Aerith.java
@@ -1,6 +1,19 @@
-public class Duke {
+import java.util.Scanner;
+
+public class Aerith {
     public static void main(String[] args) {
-        System.out.println(". ⚬ ○ ◯");
-        System.out.println("Hi, I'm Alenia!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\n. ⚬ ✧ ○\n");
+        System.out.println("✧ Greetings mage, I am Aerith! ✧\n");
+
+        String input = scanner.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println("✧ " + input + " ✧\n");
+            input = scanner.nextLine();
+        }
+
+        System.out.println("✧ I await your return. ✧");
+        System.out.println("\n○ ✧ ⚬ .\n");
     }
 }
